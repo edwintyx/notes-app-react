63e503641f8683148f7a7e67b1e59617bd1da493
Edwin <tan.edwin.yx@gmail.com>
2020-06-11 16:36:12 +0800

    Better code format

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,48 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
-    const [listNote, setListNote] = useState([])
+  const [listNote, setListNote] = useState([]);
 
-    // add note to array
-    function addNote(note) {
-        setListNote( prevNote => {
-            return [...prevNote, note];
-        })
-    }
+  // add note to array
+  function addNote(note) {
+    setListNote((prevNote) => {
+      return [...prevNote, note];
+    });
+  }
 
-    // delete note in array
-    function deleteNote(id) {
-        console.log("deleted id: " + id);
-        
-        setListNote(prevList => {
-            return prevList.filter((note, index) => {
-                return index !== id;
-            }) 
-        })
-    }
+  // delete note in array
+  function deleteNote(id) {
+    console.log("deleted id: " + id);
 
+    setListNote((prevList) => {
+      return prevList.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
 
-    return (
-        <div>
-            <Header />
-            <CreateArea 
-                addNote={addNote}
-            />
-            {listNote.map((eachNote, index) => {
-                return <Note key={index} id={index} title={eachNote.title} content={eachNote.content} delNote={deleteNote}/>
-            })}
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <CreateArea addNote={addNote} />
+      {listNote.map((eachNote, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={eachNote.title}
+            content={eachNote.content}
+            delNote={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,37 +1,47 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function CreateArea(props) {
-    const [note, setNote] = useState({
-        title: "",
-        content: ""
-    })
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
 
-    function handleChange(event) {
-        // ES6 destructuring
-        const {name, value} = event.target;
-        setNote(prev => {
-            return {
-                ...prev,
-                [name]: value
-            }
-        })
-    }
+  function handleChange(event) {
+    // ES6 destructuring
+    const { name, value } = event.target;
+    setNote((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
 
-    function onAdd(event) {
-        props.addNote(note)
-        setNote({
-            title: "",
-            content: ""
-        })
-        event.preventDefault();
-    }
-
+  function onAdd(event) {
+    props.addNote(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
 
   return (
     <div>
       <form>
-        <input onChange={handleChange} value={note.title} name="title" placeholder="Title" />
-        <textarea onChange={handleChange} value={note.content} name="content" placeholder="Take a note..." rows="3" />
+        <input
+          onChange={handleChange}
+          value={note.title}
+          name="title"
+          placeholder="Title"
+        />
+        <textarea
+          onChange={handleChange}
+          value={note.content}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
         <button onClick={onAdd}>Add</button>
       </form>
     </div>
