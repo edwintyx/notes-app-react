e83eecd8967c2febda1a48dc4bf0fe1f12e17fc8
Edwin <tan.edwin.yx@gmail.com>
2020-06-11 15:29:47 +0800

    added addNote & deleteNote functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
+    const [listNote, setListNote] = useState([])
+
+    // add note to array
+    function addNote(note) {
+        setListNote( prevNote => {
+            return [...prevNote, note];
+        })
+    }
+
+    // delete note in array
+    function deleteNote(id) {
+        console.log("deleted id: " + id);
+        
+        setListNote(prevList => {
+            return prevList.filter((note, index) => {
+                return index !== id;
+            }) 
+        })
+    }
+
+
     return (
         <div>
             <Header />
-            {notes.map(entry => <Note 
-                key={entry.key}
-                title={entry.title}
-                content={entry.content}
-            />)}
+            <CreateArea 
+                addNote={addNote}
+            />
+            {listNote.map((eachNote, index) => {
+                return <Note key={index} id={index} title={eachNote.title} content={eachNote.content} delNote={deleteNote}/>
+            })}
             <Footer />
         </div>
     );
